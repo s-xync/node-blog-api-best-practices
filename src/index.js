941e67208a7eb6f1ccad52e077ad69a3ecb575da
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-23 16:06:17 +0530

    folder structure setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`
+    🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    ---
+    Server running on port : ${PORT}
+    ---
+    Running on ${process.env.NODE_ENV}
+    ---
+    🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    `);
+  }
+});
